19897b22f9eee47cd66136d67f49501d2c9a77e1
lebrancconvas <lebrancconvas@gmail.com>
2022-11-02 09:52:45 +0700

    💡 chore(comment): add comments to explain the script

--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
 cd "$1" &&
-go build "$1".go &&
-go tool compile -S "$1".go > "$1".s &&
-./"$1" &&
+go build "$1".go && # build the execution file from go file. 
+go tool compile -S "$1".go > "$1".s && # generate go's assembly code from go file. 
+./"$1" && # run an execution file. 
 cd ..
