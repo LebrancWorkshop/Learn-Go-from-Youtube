989544ebbfd39d633bc5cc2e41d17fec14a5d111
lebrancconvas <lebrancconvas@gmail.com>
2022-11-02 18:07:30 +0700

    🔧 chore(config): update shell script

--- main.sh
+++ main.sh
@@ -1,5 +1,4 @@
 cd "$1" &&
-go build "$1".go && # build the execution file from go file. 
+go run "$1".go && # build the execution file from go file. 
 go tool compile -S "$1".go > "$1".s && # generate go's assembly code from go file. 
-./"$1" && # run an execution file. 
 cd ..
