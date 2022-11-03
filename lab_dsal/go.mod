2c339e73a1ce0ab2bcc860797ecb3d964765a36e
lebrancconvas <lebrancconvas@gmail.com>
2022-11-04 02:20:31 +0700

    ✨ feat: add go module

---
+++ lab_dsal/go.mod
@@ -0,0 +1,3 @@
+module github.com/LebrancWorkshop/Learn-Go-from-Youtube/lab_dsal
+
+go 1.19
--- lab_dsal/stack/stack.go
+++ lab_dsal/stack/stack.go
@@ -1,1 +1,62 @@
 package stack
+
+import (
+	"fmt"
+)
+
+type Stack struct {
+	size int;
+	data []int;
+	top int;
+}
+
+func Init(size int) *Stack {
+	return &Stack{
+		size: size,
+		data: make([]int, size),
+		top: -1,
+	}
+}
+
+func (s *Stack) Push(value int) {
+	if s.isFull() {
+		fmt.Println("Stack is Full.");
+	} else {
+		s.top++;
+		s.data[s.top] = value;
+	}
+}
+
+func (s *Stack) Pop() int {
+	if s.isEmpty() {
+		fmt.Println("Stack is Empty.");
+		return -1;
+	} else {
+		value := s.data[s.top];
+		s.data[s.top] = 0;
+		s.top--;
+		return value;
+	}
+}
+
+func (s *Stack) Peek() int {
+	return s.data[s.top];
+}
+
+func (s *Stack) isEmpty() bool {
+	if s.top == -1 {
+		return true;
+	}
+	return false;
+}
+
+func (s *Stack) isFull() bool {
+	if s.top == s.size - 1 {
+		return true;
+	}
+	return false;
+}
+
+func (s *Stack) Display() {
+	fmt.Println(s);
+}
